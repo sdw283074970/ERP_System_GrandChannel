71eb7ddddaf32e5be89bf88e3b3a71c01e823c42
sdw283074970 <downwes@gmail.com>
2018-07-03 22:24:58 -0700

    添加上传库位报告时UNDO的操作

---
+++ ClothResorting/Controllers/Api/LocationDetailUnDoController.cs
@@ -0,0 +1,26 @@
+﻿using ClothResorting.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ClothResorting.Controllers.Api
+{
+    public class LocationDetailUnDoController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public LocationDetailUnDoController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // DELETE /api/locaitionDetailUnDo
+        public void UnDoUpload()
+        {
+
+        }
+    }
+}
