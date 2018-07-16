25d7ff68d3a4e4c09927c5f945bff26563e65387
sdw283074970 <downwes@gmail.com>
2018-07-16 14:36:11 -0700

    解除了匿名访问者的注册权限

--- ClothResorting/Controllers/AccountController.cs
+++ ClothResorting/Controllers/AccountController.cs
@@ -136,7 +136,7 @@ namespace ClothResorting.Controllers
 
         //
         // GET: /Account/Register
-        [AllowAnonymous]
+        //[AllowAnonymous]
         public ActionResult Register()
         {
             return View();
@@ -145,7 +145,7 @@ namespace ClothResorting.Controllers
         //
         // POST: /Account/Register
         [HttpPost]
-        [AllowAnonymous]
+        //[AllowAnonymous]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Register(RegisterViewModel model)
         {
