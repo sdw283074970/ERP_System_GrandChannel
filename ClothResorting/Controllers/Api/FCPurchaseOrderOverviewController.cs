8acb9a41affa2664b553d36e51c07d7080fce227
sdw283074970 <downwes@gmail.com>
2018-07-23 11:28:11 -0700

    填充了删除FreeCountry的数据库相关表的接口。现在可以直接删除所有记录。 现在每当为PO指定集装箱号时，集装箱号会同步到预收货总览中。

--- ClothResorting/Controllers/Api/FCPurchaseOrderOverviewController.cs
+++ ClothResorting/Controllers/Api/FCPurchaseOrderOverviewController.cs
@@ -41,12 +41,10 @@ namespace ClothResorting.Controllers.Api
         [HttpPost]
         public void UploadAndExtractFreeCountryExcel()
         {
-            var fileSavePath = "";
-
             //从httpRequest中获取文件并写入磁盘系统
             var filesGetter = new FilesGetter();
 
-            fileSavePath = filesGetter.GetAndSaveFileFromHttpRequest(@"D:\TempFiles\");
+            var fileSavePath = filesGetter.GetAndSaveFileFromHttpRequest(@"D:\TempFiles\");
 
             if (fileSavePath == "")
             {
@@ -89,6 +87,83 @@ namespace ClothResorting.Controllers.Api
                     carton.Container = container;
                 }
             }
+            _context.SaveChanges();
+
+            //统计该PackingList下Container号码的数量，并将结果同步至prereceiveOrder中
+            poSummariesInDb = _context.POSummaries
+                .Include(c => c.PreReceiveOrder)
+                .Include(c => c.RegularCartonDetails)
+                .Where(c => c.PreReceiveOrder.Id == preId);
+
+            var containerList = new List<string>();
+
+            foreach(var poSummary in poSummariesInDb)
+            {
+                if (!containerList.Contains(poSummary.Container))
+                {
+                    containerList.Add(poSummary.Container);
+                }
+            }
+
+            var containerString = containerList[0];
+
+            for(int i = 1; i < containerList.Count; i++)
+            {
+                containerString += ", " + containerList[i];
+            }
+
+            var preReceiveOrderInDb = _context.PreReceiveOrders.Find(preId);
+
+            preReceiveOrderInDb.ContainerNumber = containerString;
+
+            _context.SaveChanges();
+        }
+
+        //单个删除主页中的FC相关的Prereceiverder记录
+        // DELETE /api/fcpurchaseOrderOverview/
+        [HttpDelete]
+        public void DeleteFCPreReceiveOrder([FromUri]int id)
+        {
+            var pickingRecordsInDb = _context.PickingRecords
+                .Include(c => c.FCRegularLocationDetail.PreReceiveOrder)
+                .Where(c => c.FCRegularLocationDetail.PreReceiveOrder.Id == id);
+
+            _context.PickingRecords.RemoveRange(pickingRecordsInDb);
+
+            var cartonInsidesInDb = _context.CartonInsides
+                .Include(c => c.FCRegularLocationDetail.PreReceiveOrder)
+                .Where(c => c.FCRegularLocationDetail.PreReceiveOrder.Id == id);
+
+            _context.CartonInsides.RemoveRange(cartonInsidesInDb);
+
+            var pickingListsInDb = _context.PickingLists
+                .Include(c => c.PreReceiveOrder)
+                .Where(c => c.PreReceiveOrder.Id == id);
+
+            _context.PickingLists.RemoveRange(pickingListsInDb);
+
+            var fcRegularLocationDetialsInDb = _context.FCRegularLocationDetails
+                .Include(c => c.PreReceiveOrder)
+                .Where(c => c.PreReceiveOrder.Id == id);
+
+            _context.FCRegularLocationDetails.RemoveRange(fcRegularLocationDetialsInDb);
+
+            var regularCartonDetailsInDb = _context.RegularCartonDetails
+                .Include(x => x.POSummary.PreReceiveOrder)
+                .Where(x => x.POSummary.PreReceiveOrder.Id == id);
+
+            _context.RegularCartonDetails.RemoveRange(regularCartonDetailsInDb);
+
+            var poSummariesInDb = _context.POSummaries
+                .Include(x => x.PreReceiveOrder)
+                .Where(x => x.PreReceiveOrder.Id == id);
+
+            _context.POSummaries.RemoveRange(poSummariesInDb);
+
+            var preReceiveOrderInDb = _context.PreReceiveOrders.Find(id);
+
+            _context.PreReceiveOrders.Remove(preReceiveOrderInDb);
+
             _context.SaveChanges();
         }
     }
