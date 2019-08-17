dbac2d099a84a9b6d1b93c91cde1255c5c5cd368
sdw283074970 <downwes@gmail.com>
2019-08-16 17:34:57 -0700

    成功将ZEBRA SCANNER与系统连接。

--- ClothResorting/Controllers/HomeController.cs
+++ ClothResorting/Controllers/HomeController.cs
@@ -1,12 +1,9 @@
-﻿using ClothResorting.Helpers;
-using ClothResorting.Models;
+﻿using ClothResorting.Models;
 using System.Linq;
 using System.Web.Mvc;
 using ClothResorting.Models.StaticClass;
-using ClothResorting.Helpers.DPHelper;
 using System.Data.Entity;
-using System;
-using System.Collections.Generic;
+using CoreScanner;
 
 namespace ClothResorting.Controllers
 {
@@ -15,6 +12,7 @@ namespace ClothResorting.Controllers
     {
         private ApplicationDbContext _context;
         private string _userName;
+        static CCoreScannerClass cCoreScannerClass;
 
         public HomeController()
         {
@@ -50,34 +48,62 @@ namespace ClothResorting.Controllers
 
         public ActionResult Test()
         {
-            var regularCartonDetailsInDb = _context.RegularCartonDetails
-                .Include(x => x.FCRegularLocationDetail)
-                .Where(x => x.Container == "TCNU4354482");
+            cCoreScannerClass = new CCoreScannerClass();
 
-            foreach(var r in regularCartonDetailsInDb)
-            {
-                if (r.FCRegularLocationDetail.Any())
-                {
-                    r.Status = Status.Allocated;
-                }
-                else
-                {
-                    r.Status = Status.ToBeAllocated;
-                    r.ToBeAllocatedCtns = r.Cartons;
-                    r.ToBeAllocatedPcs = r.Quantity;
-                }
-            }
+            short[] scannerTypes = new short[1]; // Scanner Types you are interested in
+            scannerTypes[0] = 1; // 1 for all scanner types
+            short numberOfScannerTypes = 1; // Size of the scannerTypes array
+            int status; // Extended API return code
 
-            _context.SaveChanges();
+            cCoreScannerClass.Open(0, scannerTypes, numberOfScannerTypes, out status);
 
-            ViewBag.Message = "Your application description page.";
+            //short numberOfScanners; // Number of scanners expect to be used
+            //int[] connectedScannerIDList = new int[255];
+            //// List of scanner IDs to be returned
+            //string outXML; //Scanner details output
+
+            //cCoreScannerClass.GetScanners(out numberOfScanners, connectedScannerIDList, out outXML, out status);
+
+            // Let's beep the beeper
+            //int opcode = 6000; // Method for Beep the beeper
+            string outXML; // Output
+            //string inXML = "<inArgs>" +
+            //        "<scannerID>2</scannerID>" + // The scanner you need to beep
+            //        "<cmdArgs>" +
+            //            "<arg-int>3</arg-int>" + // 4 high short beep pattern
+            //        "</cmdArgs>" +
+            //    "</inArgs>";
+            //cCoreScannerClass.ExecCommand(opcode, ref inXML, out outXML, out status);
+
+            // Let's set the UPC-A enable/disable
+            var opcode = 5004; // Method for Set the scanner attributes
+            string inXML = "<inArgs>" +
+                "<scannerID>1</scannerID>" +
+                // The scanner you need to get the information (above)
+                "<cmdArgs>" +
+                "<arg-xml>" +
+                "<attrib_list>" +
+                "<attribute>" +
+                "<id>1</id>" +
+                // Attribute number for UPC-A
+                "<datatype>F</datatype>" +
+                "<value>False</value>" +
+                "</attribute>" +
+                "</attrib_list>" +
+                "</arg-xml>" +
+                "</cmdArgs>" +
+                "</inArgs>";
+            cCoreScannerClass.ExecCommand(opcode, ref inXML, out outXML, out status);
+
+            ViewBag.Message = outXML;
+
+            //ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-
             ViewBag.Message = "Your contact page.";
 
             return View();
