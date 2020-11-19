5e0fadeca2fbb66c52e0dd87e069efd09dbb0cc9
sdw283074970 <downwes@gmail.com>
2020-11-18 18:14:38 -0800

    增加了中台和NS平台的调用方法

---
+++ ClothResorting/Manager/NetSuit/NetSuitManager.cs
@@ -0,0 +1,236 @@
+﻿using ClothResorting.Models.FBAModels;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+
+namespace ClothResorting.Manager.NetSuit
+{
+    public class NetSuitManager
+    {
+        public ReturnData SendTransOrderShippedRequest(FBAShipOrder order, IEnumerable<FBAPickDetailCarton> pickedCtnList)
+        {
+            var url = "https://2298410.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=251&deploy=1";
+
+            var lines = new List<TransLine>();
+
+            foreach(var p in pickedCtnList)
+            {
+                if (lines.Where(x => x.ItemNum == p.FBACartonLocation.ShipmentId).Count() != 0)
+                    continue;
+
+                lines.Add(new TransLine { 
+                    Quantity = 1,
+                    ItemNum = p.FBACartonLocation.ShipmentId
+                });
+            }
+
+            var body = new TransOrderRequestBody {
+                Data = new ShippedData {
+                    TransOrderNo = order.ShipOrderNumber,
+                    TransOrderId = order.Id.ToString(),
+                    Memo = "",
+                    TranDate = order.ReleasedDate.ToString("yyyy-MM-dd"),
+                    Lines = lines
+                }
+            };
+
+            var responseString = SendHttpRequest(url, JsonConvert.SerializeObject(body), "POST");
+
+            var responseBody = new ReturnData();
+
+            using (var input = new StringReader(responseString))
+            {
+                responseBody = JsonConvert.DeserializeObject<ReturnData>(responseString);
+            }
+
+            return responseBody;
+        }
+
+        public ReturnData SendTransOrderInboundRequest(FBAMasterOrder order)
+        {
+            var url = "https://2298410.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=248&deploy=1";
+
+            var lines = new List<TransLine>();
+
+            foreach (var d in order.FBAOrderDetails)
+            {
+                if (lines.Where(x => x.ItemNum == d.ShipmentId).Count() != 0)
+                    continue;
+
+                lines.Add(new TransLine
+                {
+                    Quantity = d.ActualQuantity,
+                    ItemNum = d.ShipmentId
+                });
+            }
+
+            var body = new TransOrderRequestBody
+            {
+                Data = new ShippedData
+                {
+                    TransOrderNo = order.Container,
+                    TransOrderId = order.Id.ToString(),
+                    Memo = "",
+                    TranDate = order.InboundDate.ToString("yyyy-MM-dd"),
+                    Lines = lines
+                }
+            };
+
+            var responseString = SendHttpRequest(url, JsonConvert.SerializeObject(body), "POST");
+
+            var responseBody = new ReturnData();
+
+            using (var input = new StringReader(responseString))
+            {
+                responseBody = JsonConvert.DeserializeObject<ReturnData>(responseString);
+            }
+
+            return responseBody;
+        }
+
+        public ReturnData SendDirectOrderShippedRequest(FBAShipOrder order, IEnumerable<FBAPickDetailCarton> pickedCtnList)
+        {
+            var url = "https://2298410.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=297&deploy=1";
+
+            var skuList = new List<Sku>();
+
+            foreach(var p in pickedCtnList)
+            {
+                skuList.Add(new Sku
+                {
+                    SkuNo = p.FBACartonLocation.ShipmentId,
+                    SkuCode = p.FBACartonLocation.ShipmentId,
+                    Qty = 1
+                });
+            }
+
+            var body = new DirectSaleOrderRequestBody
+            {
+                DeliverOrderId = order.Id.ToString(),
+                SourceId = order.Id.ToString(),
+                SourceNo = order.ShipOrderNumber,
+                TrackNo = "1",
+                SkuList = new List<Sku>()
+            };
+
+            var responseString = SendHttpRequest(url, JsonConvert.SerializeObject(body), "POST");
+
+            var responseBody = new ReturnData();
+
+            using (var input = new StringReader(responseString))
+            {
+                responseBody = JsonConvert.DeserializeObject<ReturnData>(responseString);
+            }
+
+            return responseBody;
+        }
+
+        public static string SendHttpRequest(string url, string stringifiedJsonData, string method)
+        {
+            var result = string.Empty;
+
+            //发送请求
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            var data = Encoding.UTF8.GetBytes(stringifiedJsonData);
+
+            request.Method = method;
+            request.ContentType = "application/json";
+            //request.Timeout = 20000;
+            request.KeepAlive = false;
+            request.ServicePoint.Expect100Continue = false;
+            request.ContentLength = data.Length;
+            request.Accept = "application/json";
+            request.UserAgent = "APIExplorer";
+            //request.Headers.Add("Authorization", )
+            ServicePointManager.DefaultConnectionLimit = 1000;      //提高每秒默认请求数量
+
+            using (var reqStream = request.GetRequestStream())
+            {
+                reqStream.Write(data, 0, data.Length);
+                reqStream.Close();
+            }
+
+            var response = request.GetResponse();
+
+            var stream = response.GetResponseStream();
+
+            //获取响应
+            using (var reader = new StreamReader(stream, Encoding.UTF8))
+            {
+                result = reader.ReadToEnd();
+            }
+
+            return result;
+        }
+    }
+
+    public class TransOrderRequestBody
+    {
+        public dynamic Data { get; set; }
+    }
+
+    public class TransLine
+    {
+        public string ItemNum { get; set; }
+
+        public int Quantity { get; set; }
+    }
+
+    public class ShippedData
+    {
+        public string TransOrderNo { get; set; }
+
+        public string TransOrderId { get; set; }
+
+        public string TranDate { get; set; }
+
+        public string Memo { get; set; }
+
+        public ICollection<TransLine> Lines { get; set; }
+    }
+
+    public class ReturnData
+    {
+        public string RETURN_CODE { get; set; }
+
+        public TransOrderReturnDate RETURN_DATA { get; set; }
+
+        public string RETURN_MSG { get; set; }
+    }
+
+    public class TransOrderReturnDate
+    {
+        public string TransOrderNo { get; set; }
+
+        public bool Status { get; set; }
+
+        public string ErrMsg { get; set; }
+    }
+
+    public class Sku
+    {
+        public string SkuNo { get; set; }
+
+        public string SkuCode { get; set; }
+
+        public int Qty { get; set; }
+    }
+
+    public class DirectSaleOrderRequestBody
+    {
+        public string DeliverOrderId { get; set; }
+
+        public string SourceNo { get; set; }
+
+        public string SourceId { get; set; }
+
+        public string TrackNo { get; set; }
+
+        public ICollection<Sku> SkuList { get; set; }
+    }
+}
---
+++ ClothResorting/Manager/ZT/ZTManager.cs
@@ -0,0 +1,190 @@
+﻿using ClothResorting.Models.FBAModels;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+
+namespace ClothResorting.Manager.ZT
+{
+    public class ZTManager
+    {
+        // 中台出库请求，在Ready和Release按钮按下后触发
+        public ResponseBody SendShippedOrderRequest(FBAShipOrder order)
+        {
+            var url = "http://hzero-gateway.hzero-dev.nearbyexpress.com/hitf/v2/rest/invoke?namespace=HZERO&serverCode=HORD&interfaceCode=homs-order.shipment.wmsUpdate";
+            var token = GetAccessToken();
+
+            var body = new RequestBody
+            {
+                Payload = new { ShipmentId = order.ShipOrderNumber, ShipmentStatusCode = order.Status == "Ready" ? "PICKED" : "SHIPPED", TrackingNumber = "NA", ShippingFee = "NA"}
+            };
+
+            var responseString = SendHttpRequest(url, JsonConvert.SerializeObject(body), "POST", token);
+
+            var responseBody = new ResponseBody();
+
+            using (var input = new StringReader(responseString))
+            {
+                responseBody = JsonConvert.DeserializeObject<ResponseBody>(responseString);
+            }
+
+            return responseBody;
+        }
+
+        // 中台入库请求，在入库单complete按钮按下后触发
+        public ResponseBody SendInboundCompleteRequest(FBAMasterOrder order)
+        {
+            var url = "http://hzero-gateway.hzero-dev.nearbyexpress.com/hitf/v1/rest/invoke?namespace=HZERO&serverCode=HORD&interfaceCode=homs-order.rma.returnStatus";
+            var token = GetAccessToken();
+
+            var lines = new List<Line>();
+
+            foreach(var d in order.FBAOrderDetails)
+            {
+                lines.Add(new Line { 
+                    ReturnSkuId = d.ShipmentId,
+                    RmaReturnQuantity = d.ActualQuantity
+                });
+            }
+
+            var body = new RequestBody
+            {
+                Payload = new { Lines = lines, ReturnStatus = 1, RmaCode = order.Container }
+            };
+
+            var responseString = SendHttpRequest(url, JsonConvert.SerializeObject(body), "POST", token);
+
+            var responseBody = new ResponseBody();
+
+            using (var input = new StringReader(responseString))
+            {
+                responseBody = JsonConvert.DeserializeObject<ResponseBody>(responseString);
+            }
+
+            return responseBody;
+        }
+
+        public static string SendHttpRequest(string url, string stringifiedJsonData, string method, string accessToken)
+        {
+            var result = string.Empty;
+
+            //发送请求
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            var data = Encoding.UTF8.GetBytes(stringifiedJsonData);
+
+            request.Method = method;
+            request.ContentType = "application/json";
+            //request.Timeout = 20000;
+            request.KeepAlive = false;
+            request.ServicePoint.Expect100Continue = false;
+            request.ContentLength = data.Length;
+            request.Headers.Add("Authorization", "Bearer " + accessToken);
+            request.Accept = "application/json";
+            request.UserAgent = "APIExplorer";
+            ServicePointManager.DefaultConnectionLimit = 1000;      //提高每秒默认请求数量
+
+            using (var reqStream = request.GetRequestStream())
+            {
+                reqStream.Write(data, 0, data.Length);
+                reqStream.Close();
+            }
+
+            var response = request.GetResponse();
+
+            var stream = response.GetResponseStream();
+
+            //获取响应
+            using (var reader = new StreamReader(stream, Encoding.UTF8))
+            {
+                result = reader.ReadToEnd();
+            }
+
+            return result;
+        }
+
+        public string GetAccessToken()
+        {
+            var url = "http://hzero-gateway.hzero-dev.nearbyexpress.com/oauth/oauth/token?grant_type=client_credentials&client_id=wms&client_secret=DE715D4979BBE0A778BB9A23267DCE51";
+
+            var result = string.Empty;
+
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+
+            using (var reqStream = request.GetRequestStream())
+            {
+                reqStream.Close();
+            }
+
+            var response = request.GetResponse();
+
+            var stream = response.GetResponseStream();
+
+            //获取响应
+            using (var reader = new StreamReader(stream, Encoding.UTF8))
+            {
+                result = reader.ReadToEnd();
+            }
+
+            return result;
+        }
+    }
+
+    public class RequestBody
+    {
+        public dynamic PathVariables { get; set; }
+
+        public dynamic Payload { get; set; }
+
+        public dynamic ContentType { get; set; }
+
+        public RequestBody()
+        {
+            PathVariables = new { TenantId = 0 };
+
+            ContentType = new { MimeType = "application/json", Charset = "UTF-8" };
+        }
+    }
+
+    public class Line
+    {
+        public int RmaReturnQuantity { get; set; }
+
+        public string ReturnSkuId { get; set; }
+    }
+
+    public class Payload
+    {
+        public string Msg { get; set; }
+
+        public string Code { get; set; }
+
+        public string Content { get; set; }
+
+        public bool Error { get; set; }
+    }
+
+    public class ResponseBody
+    {
+        public string Status { get; set; }
+
+        public string Message { get; set; }
+
+        public Payload Payload { get; set; }
+    }
+
+    public class TokenResponseBody
+    {
+        public string Access_token { get; set; }
+
+        public string Token_type { get; set; }
+
+        public int Expires_in { get; set; }
+
+        public string Scope { get; set; }
+    }
+}
